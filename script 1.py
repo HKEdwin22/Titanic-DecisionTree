249ac916fd431f5b2736535c048768aaa9dc9ab5
KaverH <spongyworm@gmail.com>
2023-07-28 20:30:27 +0100

    Data cleansing 2

--- script 1.py
+++ script 1.py
@@ -9,18 +9,48 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('./Data/titanic_train.csv')
 
 # Get known about the data
+print(df.head())
+
 for i in df.columns:
     print(i)
 print('Number of attributes: %d' %len(df.columns))
 print('Number of entries: %d' %len(df))
 
-# Check any empty targets
-df['Survived'].fillna('Hello', inplace=True)
-faud = df.loc[df['Survived'] == 'Hello']
-print('There are %d empty targets.' %len(faud))
+'''
+Data Preprocessing
+'''
+# Drop irrelevant attributes
+df = df.loc[:, df.columns != 'Name']
+df = df.loc[:, df.columns != 'Passenger ID']
 
-# Data cleansing
+# Count total missing values for each attribute
+dummy = df.isnull().sum()
+
+# Drop unused attribute
+df = df.loc[:, df.columns != 'Cabin']
+
+# Check any invalid format
+def check_format(x, type):
+    if type == 'num':
+        dummy = df.applymap(lambda x: isinstance(x, (int, float)))[i]
+    elif type == 'str':
+        dummy = df.applymap(lambda x: isinstance(x, (str)))[i]
+    dummy = dummy.to_frame()
+    dummy = dummy[i].value_counts()
+    print(dummy)
+    
+num_att = ['Age', 'Fare', 'Parch', 'Pclass', 'SibSp', 'Family_Size']
+str_att = ['Embarked', 'Sex', 'Ticket', 'Title']
+
+for i in num_att:
+    check_format(i, 'num')
+for i in str_att:
+    check_format(i, 'str')
+    
+# Data cleansing 1 - Drop all null data
 df.dropna(inplace=True)
+
+# Data cleansing 2 - Replace null data with mode/mean/median
 
 # Extract targets and independent variables
 X = df.loc[:, df.columns != 'Survived']
