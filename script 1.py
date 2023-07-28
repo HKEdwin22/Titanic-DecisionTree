215081fb8d80775a4c93df8fda9a91ba976748a2
KaverH <spongyworm@gmail.com>
2023-07-28 17:11:36 +0100

    preprocessing data

--- script 1.py
+++ script 1.py
@@ -2,15 +2,25 @@
 import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
 # Read data
 df = pd.read_csv('./Data/titanic_train.csv')
 
+# Get known about the data
+for i in df.columns:
+    print(i)
+print('Number of attributes: %d' %len(df.columns))
+print('Number of entries: %d' %len(df))
+
 # Check any empty targets
 df['Survived'].fillna('Hello', inplace=True)
 faud = df.loc[df['Survived'] == 'Hello']
-print('There are %d empty targets.' %faud)
+print('There are %d empty targets.' %len(faud))
+
+# Data cleansing
+df.dropna(inplace=True)
 
 # Extract targets and independent variables
 X = df.loc[:, df.columns != 'Survived']
@@ -19,14 +29,16 @@ y = df.loc[:, df.columns == 'Survived']
 # Split the dataset into training and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
 
-# Call and train the model
+# Call, train and evaluate the model
 model = tree.DecisionTreeClassifier(random_state = 0)
 model.fit(X_train, y_train)
+y_pred = model.predict(X_test, y_test)
 
-# Get known about the data
-for i in df.columns:
-    print(i)
-print('There are %d attributes.' %len(df.columns))
+accuracy = accuracy_score(y_test,y_pred)
+print('The prediction accuracy is %d.' %accuracy)
+
+
+
 
 
 pass
